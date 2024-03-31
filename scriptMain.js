5eeb265eff71521c275d49e58aad2ce844e7469c
Daniel-0111 <valeemonzon133@gmail.com>
2024-03-31 20:03:07 -0300

    agregando error.html

--- scriptMain.js
+++ scriptMain.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const searchGoogleButton = document.getElementById('searchGoogle');
     const searchLuckyButton = document.getElementById('searchLucky');
@@ -8,7 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const searchTerm = document.getElementById('searchInput').value;
         // Realizar búsqueda en Google con el término ingresado
         if (searchTerm.trim() !== '') {
-            window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(searchTerm);
+            if (searchTerm.trim().toLowerCase() === 'el amor de mi vida') {
+                // Si el término de búsqueda es "el amor de mi vida", dirigir a una página local
+                window.location.href = 'ERROR.html';
+            } else {
+                window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(searchTerm);
+            }
         }
     });
 
